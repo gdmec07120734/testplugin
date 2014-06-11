7597ad751c67986771f60c63cbbb300bdbb8fc21
lzc <495960481@qq.com>
2014-06-11 16:38:56 +0800

    aaa

---
+++ testplugin/platforms/android/bin/classes.dex.d
@@ -0,0 +1,9 @@
+C:\testplugin\testplugin\platforms\android\bin\classes.dex : \
+C:\testplugin\testplugin\platforms\android\bin\classes\cn\edu\gdmec\a0712026\testplugin\BuildConfig.class \
+C:\testplugin\testplugin\platforms\android\bin\classes\cn\edu\gdmec\a0712026\testplugin\R$attr.class \
+C:\testplugin\testplugin\platforms\android\bin\classes\cn\edu\gdmec\a0712026\testplugin\R$drawable.class \
+C:\testplugin\testplugin\platforms\android\bin\classes\cn\edu\gdmec\a0712026\testplugin\R$string.class \
+C:\testplugin\testplugin\platforms\android\bin\classes\cn\edu\gdmec\a0712026\testplugin\R$xml.class \
+C:\testplugin\testplugin\platforms\android\bin\classes\cn\edu\gdmec\a0712026\testplugin\R.class \
+C:\testplugin\testplugin\platforms\android\bin\classes\cn\edu\gdmec\a0712026\testplugin\testplugin.class \
+C:\testplugin\testplugin\platforms\android\bin\dexedLibs\classes-69f5361338b638909ac82b1eaeb7fbc2.jar \
